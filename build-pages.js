7288543afdc0c00ac1d4bd7e93865aed813f48d0
Oğuzhan Ocak <oguzhanocak@gmail.com>
2025-11-10 15:59:57 +0300

    webp image optimization

--- build-pages.js
+++ build-pages.js
@@ -206,6 +206,20 @@ function createHTMLTemplate(lang = 'en') {
     {{FOOTER}}
     
     <script src="${assetPath}dist/js/index.js"></script>
+    
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/6911e0faaadb9719591b72a3/1j9mtdl0g';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
 </body>
 </html>`;
 }
